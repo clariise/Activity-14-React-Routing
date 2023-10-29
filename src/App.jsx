e21abda5ac2565a96cd60aeb357ae76a320e5175
clariise <clcarmona001@gmail.com>
2023-10-29 14:59:18 +0800

    React Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,29 @@
-import { useState } from 'react'
-import Todo from './Todo';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Layout from "./pages/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Services from "./pages/Services.jsx"
+import NotFound from "./pages/NotFound.jsx"
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
 
-  const [todos, setTodos] = useState ([]); 
-  const [input, setInput] = useState ('');
-
-  const newTodo =() => {
-    setTodos([...todos, input]);
-    setInput('');
-
-  }
-
-
   return (
-    <>
-    <main className="container p-5 shadow">
-      <h1 className="fw-bold">📝To-do list</h1>
-      <label htmlFor="newtodo">Add a new task</label>
-      <input id="newtodo" type="text" value={input} className="form-control" placeholder='Enter your task here'onChange={(e)=> {
-        setInput(e.target.value)
-      }} />
-      <button className="btn btn-primary mt-2" onClick={newTodo} >Submit task</button>
-      <div className="card mt-3 p-3">
-        {
-
-            todos.map((todo, index) => (
-              <Todo todo= {todo} index={index} />
-            )) 
-        }
-        
-
-      </div>
-    </main>
-     
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="contact" element={<Contact/>}/>
+        <Route path="services" element={<Services/>}/>
+      </Route>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
+    </BrowserRouter>
+  
   )
 }
 
-export default App
+export default App;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,8 +3,8 @@ function Home (){
 
     return (
         <section className= "text-center">
-            <h1 >This is the <b>home</b> page</h1>
-            <p>Activiity 14: React Routing</p>
+            <h1 >This is the <b>services</b> page</h1>
+            <p>Activiity 14: React Router</p>
 
         </section>
         
